4bab504913dbe513b2ba32e1cbdae827ff95e3f8
Hristiyan Nochev <81227461+HNochev@users.noreply.github.com>
2023-05-03 23:18:55 +0300

    Upload ElectricTransportBlog Project

---
+++ Areas/RoleManage/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ElectricTransportBlog.Core.Constants;
+
+namespace ElectricTransportBlog.Areas.RoleManage.Controllers
+{
+    [Authorize(Roles = UserConstants.Administrator)]
+    [Area("RoleManage")]
+    public class BaseController : Controller
+    {
+
+    }
+}
